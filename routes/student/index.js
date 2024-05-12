90add34951b99fc522a8cb16f090fc43be745657
EMMANUEL ARAOYE <106038989+Emmanuel-002@users.noreply.github.com>
2024-05-12 08:44:46 +0100

    Update index.js

--- routes/student/index.js
+++ routes/student/index.js
@@ -12,18 +12,18 @@ import record from "./record";
 
 const router = Router();
 
-router.get("/dashboard", protectRoute("/login"), dashboard)
+router.get("/dashboard", dashboard)
 router
   .route("/signup")
   .get((req, res) => res.render("signup"))
   .post(signup);
 
-  router.get("/cbt", protectRoute("/login"), CBT.getCBT)
+  router.get("/cbt", CBT.getCBT)
   // router.get("/cbt/:id", protectRoute("/login"), CBT.getCBT)
-  router.get("/cbt/test/:id", protectRoute("/login"), test.getTest)
-  router.get("/cbt/test/record/:id", protectRoute("/login"), record)
-  router.get("/cbt/test/result/:id", protectRoute("/login"), submit.result)
-  router.post("/cbt/test/result/:id", protectRoute("/login"), submit.submit)
+  router.get("/cbt/test/:id", test.getTest)
+  router.get("/cbt/test/record/:id", record)
+  router.get("/cbt/test/result/:id", submit.result)
+  router.post("/cbt/test/result/:id", submit.submit)
   
 router.route("/login")
   .get(login.getLogin)
