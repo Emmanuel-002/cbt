6a6dd531f1b478c7522c4402241487f6b7c17eed
EMMANUEL ARAOYE <106038989+Emmanuel-002@users.noreply.github.com>
2024-05-12 08:42:00 +0100

    Update dashboard.js

--- routes/student/dashboard.js
+++ routes/student/dashboard.js
@@ -2,8 +2,8 @@ import home from "./home"
 export default (req,res)=>{
   //  if(req.session.user){
         res.render("user",{
-            username: req.session.user["fullName"],
-            user_id: req.session.user["user_id"],
+           // username: req.session.user["fullName"],
+            //user_id: req.session.user["user_id"],
             content:home.content,
         })
   //  }
